9e5632f2a37c38aae0950180a933c7203338aa78
Salvatore Olivieri <99086797+salvatoreolivieri@users.noreply.github.com>
2024-10-14 00:36:21 +0200

    standardising json responses

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -55,7 +55,7 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	if err := writeJSON(w, http.StatusCreated, post); err != nil {
+	if err := app.jsonResponse(w, http.StatusCreated, post); err != nil {
 		app.internalServerError(w, r, err)
 		return
 	}
@@ -73,7 +73,7 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
 
 	post.Comments = comments
 
-	if err := writeJSON(w, http.StatusOK, post); err != nil {
+	if err := app.jsonResponse(w, http.StatusOK, post); err != nil {
 		app.internalServerError(w, r, err)
 		return
 	}
@@ -105,7 +105,7 @@ func (app *application) deletePostHandler(w http.ResponseWriter, r *http.Request
 
 	}
 
-	if err := writeJSON(w, http.StatusNoContent, fmt.Sprintf("Delete post with id: %d", id)); err != nil {
+	if err := app.jsonResponse(w, http.StatusNoContent, fmt.Sprintf("Delete post with id: %d", id)); err != nil {
 		app.internalServerError(w, r, err)
 		return
 	}
@@ -137,7 +137,7 @@ func (app *application) updatePostHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	if err := writeJSON(w, http.StatusOK, post); err != nil {
+	if err := app.jsonResponse(w, http.StatusOK, post); err != nil {
 		app.internalServerError(w, r, err)
 		return
 	}
